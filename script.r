6acb1b1c2a0f2c33c2a98be8a91e3db9c81fdbba
Aleksandr Gorbachev <menus12@mail.ru>
2023-11-19 12:50:09 +0100

    adjusted summary by regions

--- script.r
+++ script.r
@@ -178,13 +178,10 @@ summary_by_regionName <- esim_data %>%
     mean_mark100 = mean(mark100),      # Mean of mark100
     median_mark100 = median(mark100),  # Median of mark100
     sd_mark100 = sd(mark100),          # Standard deviation of mark100
-    avg_mark100 = mean(mark100)        # Average of mark100
-summary_by_regionName
+    avg_mark100 = mean(mark100)) %>%   # Average of mark100
+  arrange(desc(count))                 # most observations per region at the top
 
-# Sort table by count of observations, most observations per region at the top
-View(summary_by_region_sorted)
-summary_by_region_sorted <- summary_by_regionName %>%
-  arrange(desc(count))         
+summary_by_regionName    
          
 # Filter the top 10 regions in terms of observations (count)
 top_regions <- esim_data %>%
